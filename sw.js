cbe4c042e47c31c178034f22d438537328a03b0d
ivi1979-crypto <ivi1979@gmail.com>
2026-09-21 06:42:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,109 @@
-const C='enruta-0-5';
+const C = 'enruta-0-6';
 
-self.addEventListener('install',e=>{
-  e.waitUntil(
-    caches.open(C).then(c=>
-      c.addAll([
-        './',
-        './index.html',
-        './styles.css',
-        './app.js',
-        './manifest.webmanifest'
-      ])
-    )
-  );
-  self.skipWaiting();
-});
 
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>
-      Promise.all(
-        keys
-          .filter(k=>k!==C)
-          .map(k=>caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
-});
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest'
+];
 
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    fetch(e.request)
-      .then(response=>{
-        const copy=response.clone();
-        caches.open(C).then(c=>c.put(e.request,copy));
-        return response;
-      })
-      .catch(()=>caches.match(e.request))
-  );
-});
+
+self.addEventListener(
+  'install',
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .open(C)
+        .then(cache =>
+          cache.addAll(ASSETS)
+        )
+
+    );
+
+
+    self.skipWaiting();
+  }
+);
+
+
+self.addEventListener(
+  'activate',
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .keys()
+        .then(keys =>
+          Promise.all(
+
+            keys
+              .filter(
+                key => key !== C
+              )
+              .map(
+                key =>
+                  caches.delete(key)
+              )
+
+          )
+        )
+
+    );
+
+
+    self.clients.claim();
+  }
+);
+
+
+self.addEventListener(
+  'fetch',
+  event => {
+
+    if (
+      event.request.method !==
+      'GET'
+    ) {
+      return;
+    }
+
+
+    event.respondWith(
+
+      fetch(event.request)
+        .then(response => {
+
+          const copy =
+            response.clone();
+
+
+          caches
+            .open(C)
+            .then(cache => {
+
+              cache.put(
+                event.request,
+                copy
+              );
+
+            });
+
+
+          return response;
+
+        })
+        .catch(() =>
+          caches.match(
+            event.request
+          )
+        )
+
+    );
+
+  }
+);
